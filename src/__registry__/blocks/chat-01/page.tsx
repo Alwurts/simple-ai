73432410e1f22a188ee6028a178be831565f8c3c
Alwurts <jota780@gmail.com>
2025-01-13 01:04:14 +0100

    Update chat-01 example

--- src/__registry__/blocks/chat-01/page.tsx
+++ src/__registry__/blocks/chat-01/page.tsx
@@ -17,7 +17,7 @@ export default function Page() {
 	return (
 		<SidebarProvider>
 			<SidebarApp />
-			<SidebarInset className="flex flex-col h-svh">
+			<SidebarInset className="flex flex-col h-screen overflow-y-auto">
 				<header className="sticky top-0 flex h-14 shrink-0 items-center gap-2 bg-background">
 					<div className="flex flex-1 items-center gap-2 px-3">
 						<SidebarTrigger />
